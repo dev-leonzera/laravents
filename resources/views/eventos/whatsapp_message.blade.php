38f81d3b892ce85e32d44900ff3921dd01b842d0
Leonardo Andrade <leonandrade22@gmail.com>
2024-09-22 21:58:53 -0300

    ajuste na mensagem enviada

---
+++ resources/views/eventos/whatsapp_message.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>WhatsApp Message</title>
+</head>
+<body>
+    <p>Olá, amado(a)! A paz do Senhor!</p>
+    <p>Estamos felizes com a sua inscrição na Escola Bíblica de Jovens 2024 com o tema Criados para a eternidade. 🙏🏻🎉</p>
+    <p>Para confirmarmos a sua participação, estamos enviando o link de pagamento para o evento:</p>
+    <p><a href="{{ $paymentLink }}">{{ $paymentLink }}</a></p>
+    <p>O pagamento pode ser realizado por pix ou cartão de crédito.</p>
+</body>
+</html>
